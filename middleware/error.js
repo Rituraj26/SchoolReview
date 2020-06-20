0ba0dd7e0862102d02bd464e467acc42d8638351
Rituraj <riturajguwahati26@gmail.com>
2020-06-20 23:30:53 +0530

    implemented express error handler

--- middleware/error.js
+++ middleware/error.js
@@ -21,6 +21,12 @@ const errorHandler = (err, req, res, next) => {
         error = new ErrorResponse(message, 400);
     }
 
+    // Mongoose validation error
+    // console.log(Object.values(err.errors));
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map((val) => val.message);
+        error = new ErrorResponse(message, 400);
+    }
     res.status(error.statusCode || 500).json({
         success: false,
         error: error.message || 'Server Error',
