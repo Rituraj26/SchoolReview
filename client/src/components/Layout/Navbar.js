0b3e5bbc57335fe0ea4f4d224a6bd2faec2afde7
Rituraj <riturajguwahati26@gmail.com>
2020-07-08 03:17:06 +0530

    logout and navbar links

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 
-const Navbar = () => {
-    return <div>navbar</div>;
+import { logout } from '../../actions/auth';
+
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+    const authLinks = (
+        <span>
+            <a href="#!" onClick={logout}>
+                Logout
+            </a>
+        </span>
+    );
+
+    const guestLinks = (
+        <span>
+            <Link to="/auth/login">Login</Link>
+            <Link to="/auth/register">Sign In</Link>
+        </span>
+    );
+
+    return (
+        <nav>
+            <Link to="/">DevConnector</Link>
+
+            {!loading && (
+                <Fragment> {isAuthenticated ? authLinks : guestLinks}</Fragment>
+            )}
+        </nav>
+    );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+    logout: propTypes.func.isRequired,
+    auth: propTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => {
+    return {
+        auth: state.auth,
+    };
+};
+
+export default connect(mapStateToProps, { logout })(Navbar);
