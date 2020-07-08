448148303cb329e443b79013265ddbf72bb9b200
Rituraj <riturajguwahati26@gmail.com>
2020-07-08 17:45:20 +0530

    register component design

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -33,46 +33,147 @@ const Register = ({ register, isAuthenticated }) => {
 
     return (
         <Fragment>
-            <form onSubmit={(e) => onSubmit(e)}>
-                <input
-                    type="text"
-                    placeholder="name"
-                    name="name"
-                    value={name}
-                    required
-                    onChange={(e) => onChange(e)}
-                />
-                <input
-                    type="text"
-                    placeholder="email"
-                    name="email"
-                    value={email}
-                    required
-                    onChange={(e) => onChange(e)}
-                />
-                <select value={role} name="role" onChange={(e) => onChange(e)}>
-                    <option value="user">User</option>
-                    <option value="teacher">Teacher</option>
-                    <option value="publisher">Publisher</option>
-                </select>
+            <section className="form mt-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 m-auto">
+                            <div className="card bg-white p-4 mb-4">
+                                <div className="card-body">
+                                    <h1>
+                                        <i className="fas fa-user-plus"></i>{' '}
+                                        Register
+                                    </h1>
+                                    <p>
+                                        Register to list your bootcamp or rate,
+                                        review and favorite bootcamps
+                                    </p>
+                                    <form onSubmit={(e) => onSubmit(e)}>
+                                        <div className="form-group">
+                                            <label htmlFor="name">Name</label>
+                                            <input
+                                                type="text"
+                                                id="name"
+                                                name="name"
+                                                value={name}
+                                                className="form-control"
+                                                placeholder="Enter full name"
+                                                onChange={(e) => onChange(e)}
+                                                required
+                                            />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="email">
+                                                Email Address
+                                            </label>
+                                            <input
+                                                type="email"
+                                                id="email"
+                                                name="email"
+                                                value={email}
+                                                className="form-control"
+                                                placeholder="Enter email"
+                                                onChange={(e) => onChange(e)}
+                                                required
+                                            />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="password">
+                                                Password
+                                            </label>
+                                            <input
+                                                type="password"
+                                                id="password"
+                                                name="password"
+                                                value={password}
+                                                className="form-control"
+                                                placeholder="Enter password"
+                                                onChange={(e) => onChange(e)}
+                                                required
+                                            />
+                                        </div>
 
-                <input
-                    type="password"
-                    placeholder="password"
-                    name="password"
-                    value={password}
-                    required
-                    onChange={(e) => onChange(e)}
-                />
-                <input
-                    type="submit"
-                    className="btn btn-primary"
-                    value="Register"
-                />
-            </form>
-            <p className="my-1">
-                Already have an account? <Link to="/auth/login">Login</Link>
-            </p>
+                                        <div className="card card-body mb-3">
+                                            <h5>User Role</h5>
+                                            <div className="form-check">
+                                                <input
+                                                    className="form-check-input"
+                                                    type="radio"
+                                                    id="user"
+                                                    name="role"
+                                                    value="user"
+                                                    onChange={(e) =>
+                                                        onChange(e)
+                                                    }
+                                                    checked
+                                                />
+                                                <label
+                                                    className="form-check-label"
+                                                    htmlFor="user"
+                                                >
+                                                    Regular User (Browse, Write
+                                                    reviews, etc)
+                                                </label>
+                                            </div>
+                                            <div className="form-check">
+                                                <input
+                                                    className="form-check-input"
+                                                    type="radio"
+                                                    id="teacher"
+                                                    name="role"
+                                                    value="teacher"
+                                                    onChange={(e) =>
+                                                        onChange(e)
+                                                    }
+                                                />
+                                                <label
+                                                    className="form-check-label"
+                                                    htmlFor="teacher"
+                                                >
+                                                    School Teacher
+                                                </label>
+                                            </div>
+                                            <div className="form-check">
+                                                <input
+                                                    className="form-check-input"
+                                                    type="radio"
+                                                    id="publisher"
+                                                    name="role"
+                                                    value="publisher"
+                                                    onChange={(e) =>
+                                                        onChange(e)
+                                                    }
+                                                />
+                                                <label
+                                                    className="form-check-label"
+                                                    htmlFor="publisher"
+                                                >
+                                                    School Publisher
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <p className="text-danger">
+                                            * You must be affiliated with the
+                                            bootcamp in some way in order to add
+                                            it to DevCamper.
+                                        </p>
+                                        <div className="form-group">
+                                            <input
+                                                type="submit"
+                                                value="Register"
+                                                className="btn btn-primary btn-block"
+                                            />
+                                        </div>
+                                    </form>
+                                    <p className="my-1">
+                                        Already have an account?{' '}
+                                        <Link to="/auth/login">Login</Link>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </Fragment>
     );
 };
