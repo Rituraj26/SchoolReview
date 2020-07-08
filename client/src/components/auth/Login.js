75d43dc11e5d45339ecd05a6ef9ffa4e9a80c0be
Rituraj <riturajguwahati26@gmail.com>
2020-07-08 17:24:24 +0530

    login component design

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -31,32 +31,74 @@ const Login = ({ login, isAuthenticated }) => {
 
     return (
         <Fragment>
-            <form onSubmit={(e) => onSubmit(e)}>
-                <input
-                    type="text"
-                    placeholder="email"
-                    name="email"
-                    value={email}
-                    required
-                    onChange={(e) => onChange(e)}
-                />
-                <input
-                    type="password"
-                    placeholder="password"
-                    name="password"
-                    value={password}
-                    required
-                    onChange={(e) => onChange(e)}
-                />
-                <input
-                    type="submit"
-                    className="btn btn-primary"
-                    value="Login"
-                />
-            </form>
-            <p className="my-1">
-                Don't have an account? <Link to="/auth/register">Sign In</Link>
-            </p>
+            <section className="form mt-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 m-auto">
+                            <div className="card bg-white p-4 mb-4">
+                                <div className="card-body">
+                                    <h1>
+                                        <i className="fas fa-sign-in-alt"></i>{' '}
+                                        Login
+                                    </h1>
+                                    <p>
+                                        Log in to list your bootcamp or rate,
+                                        review and favorite bootcamps
+                                    </p>
+                                    <form onSubmit={(e) => onSubmit(e)}>
+                                        <div className="form-group">
+                                            <label htmlFor="email">
+                                                Email Address
+                                            </label>
+                                            <input
+                                                type="email"
+                                                className="form-control"
+                                                placeholder="Enter email"
+                                                name="email"
+                                                value={email}
+                                                required
+                                                onChange={(e) => onChange(e)}
+                                            />
+                                        </div>
+                                        <div className="form-group mb-4">
+                                            <label htmlFor="password">
+                                                Password
+                                            </label>
+                                            <input
+                                                type="password"
+                                                name="password"
+                                                value={password}
+                                                className="form-control"
+                                                placeholder="Enter password"
+                                                onChange={(e) => onChange(e)}
+                                                required
+                                            />
+                                        </div>
+                                        <div className="form-group">
+                                            <input
+                                                type="submit"
+                                                value="Login"
+                                                className="btn btn-primary btn-block"
+                                            />
+                                        </div>
+                                    </form>
+                                    <p>
+                                        {' '}
+                                        Forgot Password?{' '}
+                                        <a href="reset-password.html">
+                                            Reset Password
+                                        </a>
+                                    </p>
+                                    <p>
+                                        Don't have an account?{' '}
+                                        <Link to="/auth/register">Sign In</Link>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </Fragment>
     );
 };
