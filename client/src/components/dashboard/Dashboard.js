6cd5cab3eaf369c1e3ae7d3616890bf30f7201fe
Rituraj <riturajguwahati26@gmail.com>
2020-07-13 01:53:02 +0530

    refactored Dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
 import Schools from '../schools/Schools';
 import AllTeachers from './AllTeachers';
 import ManageAccount from './ManageAccount';
@@ -9,122 +11,14 @@ import ManageSchool from './ManageSchool';
 import ManageTeachers from './ManageTeachers';
 import ManageReviews from './ManageReviews';
 
-const Dashboard = (props) => {
+const Dashboard = () => {
     return (
         <Router>
             <div className="d-flex" id="wrapper">
-                <div
-                    className="bg-light border-right shadow-sm "
-                    id="sidebar-wrapper"
-                >
-                    <div className="sidebar-heading">Dashboard </div>
-                    <div className="list-group list-group-flush">
-                        <Link
-                            to="/dashboard/allschools"
-                            className="list-group-item list-group-item-action bg-light"
-                        >
-                            All Schools
-                        </Link>
-                        <Link
-                            to="/dashboard/allteachers"
-                            className="list-group-item list-group-item-action bg-light"
-                        >
-                            All Teachers
-                        </Link>
-                        <Link
-                            to="/dashboard/account"
-                            className="list-group-item list-group-item-action bg-light"
-                        >
-                            Manage Account
-                        </Link>
-                        <Link
-                            to="/dashboard/school"
-                            className="list-group-item list-group-item-action bg-light"
-                        >
-                            Manage School
-                        </Link>
-                        <Link
-                            to="/dashboard/teachers"
-                            className="list-group-item list-group-item-action bg-light"
-                        >
-                            Manage Teachers
-                        </Link>
-                        <Link
-                            to="/dashboard/reviews"
-                            className="list-group-item list-group-item-action bg-light"
-                        >
-                            Manage Reviews
-                        </Link>
-                    </div>
-                </div>
+                <Sidebar />
 
                 <div id="page-content-wrapper">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                        <button className="btn btn-primary" id="menu-toggle">
-                            Toggle Menu
-                        </button>
-
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div
-                            className="collapse navbar-collapse"
-                            id="navbarSupportedContent"
-                        >
-                            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                                <li className="nav-item active">
-                                    <a className="nav-link" href="#">
-                                        Home{' '}
-                                        <span className="sr-only">
-                                            (current)
-                                        </span>
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">
-                                        Link
-                                    </a>
-                                </li>
-                                <li className="nav-item dropdown">
-                                    <a
-                                        className="nav-link dropdown-toggle"
-                                        href="#"
-                                        id="navbarDropdown"
-                                        role="button"
-                                        data-toggle="dropdown"
-                                        aria-haspopup="true"
-                                        aria-expanded="false"
-                                    >
-                                        Dropdown
-                                    </a>
-                                    <div
-                                        className="dropdown-menu dropdown-menu-right"
-                                        aria-labelledby="navbarDropdown"
-                                    >
-                                        <a className="dropdown-item" href="#">
-                                            Action
-                                        </a>
-                                        <a className="dropdown-item" href="#">
-                                            Another action
-                                        </a>
-                                        <div className="dropdown-divider"></div>
-                                        <a className="dropdown-item" href="#">
-                                            Something else here
-                                        </a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
+                    <Navbar />
 
                     <Switch>
                         <Route
---
+++ client/src/components/dashboard/Sidebar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+    return (
+        <div className="bg-light border-right shadow-sm " id="sidebar-wrapper">
+            <div className="sidebar-heading">Dashboard </div>
+            <div className="list-group list-group-flush">
+                <Link
+                    to="/dashboard/allschools"
+                    className="list-group-item list-group-item-action bg-light"
+                >
+                    All Schools
+                </Link>
+                <Link
+                    to="/dashboard/allteachers"
+                    className="list-group-item list-group-item-action bg-light"
+                >
+                    All Teachers
+                </Link>
+                <Link
+                    to="/dashboard/account"
+                    className="list-group-item list-group-item-action bg-light"
+                >
+                    Manage Account
+                </Link>
+                <Link
+                    to="/dashboard/school"
+                    className="list-group-item list-group-item-action bg-light"
+                >
+                    Manage School
+                </Link>
+                <Link
+                    to="/dashboard/teachers"
+                    className="list-group-item list-group-item-action bg-light"
+                >
+                    Manage Teachers
+                </Link>
+                <Link
+                    to="/dashboard/reviews"
+                    className="list-group-item list-group-item-action bg-light"
+                >
+                    Manage Reviews
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
