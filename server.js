d116982286aec032bbf5e9e725b71755988bf4f4
Rituraj <riturajguwahati26@gmail.com>
2020-07-23 03:51:35 +0530

    final commit before production

--- server.js
+++ server.js
@@ -75,6 +75,16 @@ app.use('/reviews', reviews);
 // Using Error Handler
 app.use(errorHandler);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 
 // Start Server
