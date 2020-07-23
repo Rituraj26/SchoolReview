3b730d9875d691cf4c63243da10abbdfb8ab5fac
Rituraj <riturajguwahati26@gmail.com>
2020-07-23 18:45:38 +0530

    added config file for production

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 
 // Load env var
-dotenv.config({ path: './config/config.env' });
+// dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: './config/configProd.env' });
 
 // Connect Database
 connectDB();
