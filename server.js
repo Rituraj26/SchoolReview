f2b2586ce6f40561c8928d6c6b768235dd8aecae
Rituraj <riturajguwahati26@gmail.com>
2020-07-24 13:21:47 +0530

    commented build gitignore

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use(fileUpload());
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Cookie Parser middleware
 app.use(cookieParser());
